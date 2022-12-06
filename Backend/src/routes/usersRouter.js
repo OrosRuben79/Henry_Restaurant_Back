82a50f3f6c01967153caac44629c7f4c24bb087f
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2022-12-06 09:55:09 -0800

    Registro de usuarios CRUD BASE completo

--- Backend/src/routes/usersRouter.js
+++ Backend/src/routes/usersRouter.js
@@ -1,7 +1,10 @@
-const { Router } = require('express');
-const { getUsers } = require('../controllers/usersContoller')
+const { Router } = require("express");
+const { getUser, postUser, putUser,delteUser } = require("../controllers/usersContoller");
 const router = Router();
 
-router.get('/', getUsers)
+router.get("/", getUser);
+router.post("/", postUser);
+router.put("/:id", putUser);
+router.delete("/:id", delteUser);
 
-module.exports = router
+module.exports = router;
