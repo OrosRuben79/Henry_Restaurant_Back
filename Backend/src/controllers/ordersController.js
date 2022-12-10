99775b3725a54cc34e19dcd0725e2512d92b3619
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2022-12-10 03:27:53 -0300

    agrego ruta admins y order

--- Backend/src/controllers/ordersController.js
+++ Backend/src/controllers/ordersController.js
@@ -1,6 +1,18 @@
-const getOrders = (req, res) => {
-    res.json({ msg: "respuesta deade el controlador getOrders" });
+const Order = require("../models/order");
+
+const getOrders = async (req, res) => {
+
+
+  try {
+    const order = await Order.find().populate('fullName');
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({ msg: error });
+  }
   };
+
+
+
   module.exports = {
     getOrders,
   };
--- Backend/src/routes/adminsRouter.js
+++ Backend/src/routes/adminsRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
-const { getAdmins } = require('../controllers/adminsController')
+const { getAdmins, postAdmins, putAdmins, deleteAdmins } = require('../controllers/adminsController')
 const router = Router();
 
 router.get('/', getAdmins)
+router.post('/', postAdmins)
+router.put('/:id', putAdmins)
+router.delete('/:id', deleteAdmins)
 
 module.exports = router
