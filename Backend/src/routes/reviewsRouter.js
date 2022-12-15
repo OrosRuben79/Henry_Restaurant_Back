f11ea23eee55da3ba278c727bfb619f79cefc442
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2022-12-14 23:56:33 -0300

    agrego controller borrador logico en reviews

--- Backend/src/routes/reviewsRouter.js
+++ Backend/src/routes/reviewsRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { getReviews, postReviews, putReviews } = require('../controllers/reviewsController')
+const { getReviews, postReviews, putReviews, deleteReviews } = require('../controllers/reviewsController')
 const router = Router();
 
 router.get('/', getReviews)
 router.post('/', postReviews)
 router.put('/:id', putReviews)
-// router.delete('/:id', deleteReviews)
+router.delete('/:id', deleteReviews)
 
 module.exports = router
