43afe2f33e8307057df21aa6ddc26afd2c45f835
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2022-12-22 08:52:29 -0300

    corrijo en index - el console.log es err

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+
 const server = require("./src/app");
 const { dbConnection } = require("./src/dataBase/config");
 const port = process.env.PORT || 0;
 
 const myApp = server.listen(port, (err) => {
 	if(err){
-		return console.error(error)
+		return console.error(err)
 	} 
   console.log("Server listening at ", myApp.address().port);  
 });
