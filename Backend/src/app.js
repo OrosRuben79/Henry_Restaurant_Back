9bfaaa41edd6164f670ea2fde125c21b334cb8f2
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2023-01-06 14:23:28 -0800

    heder actulizado

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -39,13 +39,15 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors());
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000' ); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Origin', 'https://henry-client-ecvn.vercel.app'  ); // update to match the domain you will make the request from
-    // res.header('Access-Control-Allow-Origin', 'https://prueba-deploy-sable.vercel.app'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE');
-    next();
+  const allowedOrigins = ['http://localhost:3000/', 'https://henry-client-ecvn.vercel.app/']; // update to match the domain you will make the request from
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, PUT, DELETE');
+  next();
   });
 
 
